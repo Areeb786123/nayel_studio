8cce3ae66e8d9e605cfe95e1ba9cf86f4b08dbf5
Areeb786123 <areebrockzz786@gmail.com>
2025-01-05 23:24:52 +0530

    fix video

--- src/components/ImageWritter.jsx
+++ src/components/ImageWritter.jsx
@@ -4,13 +4,22 @@ import background from '../assest/footerbg.mp4'
 export const ImageWritter = () => {
   return (
     <div className="w-full h-screen bg-white min-h-screen flex items-center justify-center p-4 video-background relative rounded-md">
-    <div className="w-full h-full p-[40px] mt-[20px] rounded-lg relative rounded-md">
-        <video autoPlay loop muted className='w-full h-full'>
-            <source src={background} type='video/mp4'/>
-            test
+      <div className="w-full h-full p-4 sm:p-6 md:p-8 mt-4 sm:mt-6 md:mt-8 rounded-lg relative">
+        <video
+          autoPlay
+          loop
+          muted
+          className="w-full h-full object-cover rounded-md"
+          style={{
+            maxHeight: '100%',
+            maxWidth: '100%',
+            borderRadius: '10px',
+          }}
+        >
+          <source src={background} type="video/mp4" />
+          Your browser does not support the video tag.
         </video>
-  </div>
-  </div>
-  
-  )
-}
+      </div>
+    </div>
+  );
+};
